3eaf3d776892522f05c751fa00d04999317095a7
Tatiana Kashcha <tkashcha@gmail.com>
2023-08-11 18:58:01 +0300

    add case

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("Welcome to Node.js");
+// const argv = require("yargs").argv;
+
+// const yargs = require("yargs");
+// const { hideBin } = require("yargs/helpers");
+
+const contacts = require("./contacts");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      return console.log(allContacts);
+
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      return console.log(oneContact);
+
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+      return console.log(newContact);
+
+    case "remove":
+      const deleteContact = await contacts.removeContact(id);
+      return console.log(deleteContact);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+// invokeAction({ action: "list" });
+
+// const arr = hideBin(process.argv);
+// const { argv } = yargs(arr);
+// console.log(argv);
+// invokeAction(argv);
