ea8ecb0f38acafd6f9fc79c9051800f9d5822aa0
Tatiana Kashcha <tkashcha@gmail.com>
2023-08-12 19:41:50 +0300

    upd console

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
     case "list":
       const allContacts = await contacts.listContacts();
-      return console.log(allContacts);
+      return console.table(allContacts);
 
     case "get":
       const oneContact = await contacts.getContactById(id);
-      return console.log(oneContact);
+      return console.table(oneContact);
 
     case "add":
       const newContact = await contacts.addContact({ name, email, phone });
-      return console.log(newContact);
+      return console.table(newContact);
 
     case "remove":
       const deleteContact = await contacts.removeContact(id);
-      return console.log(deleteContact);
+      return console.table(deleteContact);
 
     default:
       console.warn("\x1B[31m Unknown action type!");
